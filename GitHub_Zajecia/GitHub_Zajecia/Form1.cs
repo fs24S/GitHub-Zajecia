63836f8e3ebdf545c65c9fac4b1e511602442548
Szopen <student@SWML-HP04>
2026-03-24 14:57:12 +0100

    nowe

--- GitHub_Zajecia/GitHub_Zajecia/Form1.cs
+++ GitHub_Zajecia/GitHub_Zajecia/Form1.cs
@@ -24,6 +24,7 @@ namespace GitHub_Zajecia
             students = new List<Student>();
             students.Add(student1);
             student4 = new Student("Alek", "Szopinski", "12369");
+            students.Add(student4);
             student10 = new Student("Filip", "Sudak", "A1");
             students.Add(student10);
             student5 = new Student("Ambroży", "Trytytka", "pierwsza");
