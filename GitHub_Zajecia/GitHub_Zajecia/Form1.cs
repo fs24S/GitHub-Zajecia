ce1ca5f0e0274afcb669dba71b2909f301d81fee
WML25\kamil.rajkowski <kamil.rajkowski@wat.edu.pl>
2026-03-24 14:38:24 +0100

    dodanie studenta Adam Mickiewicz

--- GitHub_Zajecia/GitHub_Zajecia/Form1.cs
+++ GitHub_Zajecia/GitHub_Zajecia/Form1.cs
@@ -2,10 +2,14 @@ namespace GitHub_Zajecia
 {
     public partial class Form1 : Form
     {
-        List<Student> students = new List<Student>();
+        List<Student> students;
+        Student student1;
         public Form1()
         {
             InitializeComponent();
+            student1 = new Student("Adam", "Mickiewicz", "A1");
+            students = new List<Student>();
+            students.Add(student1);
         }
 
         private void button1_Click(object sender, EventArgs e)
