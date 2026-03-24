a045794e98cec4d91798df8294aa67c98b251193
WML25\kamil.rajkowski <kamil.rajkowski@wat.edu.pl>
2026-03-24 14:22:37 +0100

    przygotowane do zjęć

--- GitHub_Zajecia/GitHub_Zajecia/Form1.cs
+++ GitHub_Zajecia/GitHub_Zajecia/Form1.cs
@@ -2,9 +2,18 @@ namespace GitHub_Zajecia
 {
     public partial class Form1 : Form
     {
+        List<Student> students = new List<Student>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var s in students)
+            {
+                MessageBox.Show(s.ToString());
+            }
         }
     }
 }
---
+++ GitHub_Zajecia/GitHub_Zajecia/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHub_Zajecia
+{
+    public class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string NrGrupy { get; set; }
+
+        public Student(string imie, string nazwisko, string nrGrupy)
+        {
+            Imie = imie;
+            Nazwisko= nazwisko;
+            NrGrupy = nrGrupy;
+        }
+
+        public override string ToString()
+        {
+            return $"{Imie} {Nazwisko} - grupa: {NrGrupy}";
+        }
+    }
+}
