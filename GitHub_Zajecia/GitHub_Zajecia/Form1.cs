65d608ca10fa02ac07e1b489c27ba263e7362714
Sudak <78984FS@SWML-HP03>
2026-03-24 14:36:46 +0100

    dodano studenta

--- GitHub_Zajecia/GitHub_Zajecia/Form1.cs
+++ GitHub_Zajecia/GitHub_Zajecia/Form1.cs
@@ -3,9 +3,12 @@ namespace GitHub_Zajecia
     public partial class Form1 : Form
     {
         List<Student> students = new List<Student>();
+
+        Student student1;
         public Form1()
         {
             InitializeComponent();
+            student1 = new Student("Jan", "Kowalski", "123");
         }
 
         private void button1_Click(object sender, EventArgs e)
