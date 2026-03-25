f2052827dcba92e1adfbe2c53cf850e5fa80075f
Szopen <alek@ALEK>
2026-03-25 16:14:42 +0100

    Okno Montaż

--- GitHub_Zajecia/GitHub_Zajecia/Form1.cs
+++ GitHub_Zajecia/GitHub_Zajecia/Form1.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
 namespace GitHub_Zajecia
 {
-    
     public partial class Form1 : Form
     {
+        // 1. Twoje stare zmienne i lista studentów
         List<Student> students;
         Student student1;
         Student student2;
@@ -10,35 +15,49 @@ namespace GitHub_Zajecia
         Student student4;
         Student studentjd;
         Student student7;
-
         Student student5;
         Student student8;
         Student student10;
+
+        // G³ówny konstruktor okienka
         public Form1()
         {
             InitializeComponent();
+
+            // 2. Twój stary kod z dodawaniem studentów
             student1 = new Student("Adam", "Mickiewicz", "A1");
-            student2 = new Student("Jan", "Kowalski", "123"); 
+            student2 = new Student("Jan", "Kowalski", "123");
             student7 = new Student("Pawel", "Sobolewski", "321");
+
             students = new List<Student>();
             students.Add(student1);
+
             student2 = new Student("Jan", "Kowalski", "123");
             student3 = new Student("Szymon", "Kepniak", "12311111");
-            student5 = new Student("Micha³", "Kurowski", "B2");
+            student5 = new Student("Michal", "Kurowski", "B2");
             studentjd = new Student("Jakub", "Dabrowski", "XD");
-            student8 = new Student("Micha³", "Kurowski", "B2");
+            student8 = new Student("Michal", "Kurowski", "B2");
+
             students = new List<Student>();
             students.Add(student1);
             students.Add(student3);
             students.Add(student8);
             students.Add(student7);
+
             student4 = new Student("Alek", "Szopinski", "12369");
             students.Add(student4);
+
             student10 = new Student("Filip", "Sudak", "A1");
             students.Add(student10);
             students.Add(student5);
             students.Add(studentjd);
-            
+
+
+            DB baza = new DB();
+            var dane = baza.PobierzDaneZTabeli("dbo.Montaz");
+
+            dataGridView1.DataSource = dane;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
